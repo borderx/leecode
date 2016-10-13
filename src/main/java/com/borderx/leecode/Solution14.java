c8f7bc92b059a1dec7353de90eb0844b70c3859d
borderx <jiangyouqizai@163.com>
2016-10-14 01:33:19 +0800

    add problem:14. Longest Common Prefix
    1.add problem:14. Longest Common Prefix
    2.add .gitignore

---
+++ src/main/java/com/borderx/leecode/Solution14.java
@@ -0,0 +1,32 @@
+package com.borderx.leecode;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Write a function to find the longest common prefix string amongst an array of strings. 
+ * 
+ * @author borderx
+ *
+ */
+public class Solution14 {
+	public String longestCommonPrefix(String[] strs) {
+	    if(strs == null || strs.length == 0)    return "";
+	    String pre = strs[0];
+	    int i = 1;
+	    while(i < strs.length){
+	        while(strs[i].indexOf(pre) != 0)
+	            pre = pre.substring(0,pre.length()-1);
+	        i++;
+	    }
+	    return pre;
+	}
+	
+	@Test
+	public void test(){
+		String[] strs = new String[]{"apple","applu","appdd","apkiu","apojh","ap"};
+		String prefix = longestCommonPrefix(strs);
+		Assert.assertEquals("ap", prefix);
+	}
+	
+}
